459b1de6ebd3a4639347e22542ffdd25facae4a3
DallonF <dallonfdev@gmail.com>
2012-08-02 10:06:06 -0700

    Set NODE_PATH to allow custom resources loading deployd module

--- deployd/bin/dpd.cmd
+++ deployd/bin/dpd.cmd
@@ -3,6 +3,8 @@
 set node="%~dp0\..\tools\node.exe"
 set script="%~dp0\..\node_modules\deployd\bin\dpd"
 
+set NODE_PATH=%~dp0\..\node_modules;$NODE_PATH;
+
 @IF EXIST %node% (
   %node% %script% -m "%~dp0\..\tools\mongod.exe" %*
 ) ELSE (
